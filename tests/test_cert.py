68074e4fc863e0a9219a35318a4942bf30284ddb
Calvin Remsburg <cremsburg.dev@gmail.com>
2023-04-17 07:50:54 -0500

    update tests

--- tests/test_cert.py
+++ tests/test_cert.py
@@ -1,8 +1,7 @@
 import os
 import pytest
 from get_certificate_chain import (
-    check_domain,
-    normalize_subject,
+    SSLCertificateChainDownloader,
 )
 
 
@@ -33,18 +32,24 @@ def cert_data(root_ca_cert, server_cert):
 
 
 def test_check_domain():
+    downloader = SSLCertificateChainDownloader()
+
     # Test without port
-    domain = check_domain("www.google.com")
+    downloader.domain = "www.google.com"
+    domain = downloader.check_domain()
     assert domain == {"hostname": "www.google.com", "port": 443}
 
     # Test with port
-    domain = check_domain("www.google.com:8443")
+    downloader.domain = "www.google.com:8443"
+    domain = downloader.check_domain()
     assert domain == {"hostname": "www.google.com", "port": 8443}
 
 
 def test_normalize_subject():
+    downloader = SSLCertificateChainDownloader()
+
     subject = "/C=US/ST=California/L=Mountain View/O=Google LLC/CN=www.google.com"
     assert (
-        normalize_subject(subject)
+        downloader.normalize_subject(subject)
         == "C_US_ST_California_L_Mountain_View_O_Google_LLC_CN_www_google_com"
     )
